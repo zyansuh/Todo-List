384e71d2d0077d29621e59f0c41fe39db70ccd77
zaynsuh <tjworud123@gmail.com>
2025-04-20 11:52:34 +0900

    홈 페이지 SSR 강제 설정으로 useSearchParams() 에러 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+export const dynamic = 'force-dynamic'
+
 import FilterTabs from '@/components/FilterTabs'
 import TodoForm from '@/components/TodoForm'
 import TodoList from '@/components/TodoList'
@@ -5,10 +8,10 @@ import ThemeToggle from '@/components/ThemeToggle'
 
 export default function HomePage() {
   return (
-    <main className="p-6">
+    <main className="p-6 max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">📝 투두 리스트</h1>
-      <FilterTabs />
       <ThemeToggle />
+      <FilterTabs />
       <TodoForm />
       <TodoList />
     </main>
